6c1f9f75a6b46fb75ac0b782c28e5e6401aeb709
yiningyan <yiningyan@tencent.com>
2024-03-22 21:21:48 +0800

    feat:添加workflow

--- tencent.ts
+++ tencent.ts
@@ -3,17 +3,10 @@ import COS from "cos-nodejs-sdk-v5/index"
 
 export class TencentUploader {
     settings: PluginSettings;
-
-    lastToken: string;
-    tokenCreated: number;
-    tokenExpires: number;
     cos: COS;
 
     constructor(settings: PluginSettings) {
         this.settings = settings;
-        this.lastToken = "";
-        this.tokenCreated = Date.now();
-        this.tokenExpires = 7200;
         this.cos = new COS({
             SecretId: settings.secretID,
             SecretKey: settings.secretKey
@@ -30,7 +23,10 @@ export class TencentUploader {
                     Key: name,
                     Body: Buffer.from(content)
                 }
-            ).catch(err => {
+            ).then(data => {
+                resolve("success")
+            })
+            .catch(err => {
                 reject(err)
             })
         })
